ead8f988b54ebd0db02395c2e940660f1b912361
Steve Brokaw <brokaw@twocanoes.com>
2017-12-19 14:48:56 -0600

    Initial Commit

---
+++ CryptoHash/CryptoHash.h
@@ -0,0 +1,19 @@
+//
+//  CryptoHash.h
+//  CryptoHash
+//
+//  Created by Steve Brokaw on 12/19/17.
+//  Copyright © 2017 techmecanique. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CryptoHash.
+FOUNDATION_EXPORT double CryptoHashVersionNumber;
+
+//! Project version string for CryptoHash.
+FOUNDATION_EXPORT const unsigned char CryptoHashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoHash/PublicHeader.h>
+
+

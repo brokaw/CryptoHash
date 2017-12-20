3e4deec2c3c81794c3b79ed7be16772b5b8b4393
Steve Brokaw <brokaw@me.com>
2017-12-19 20:06:16 -0600

    Basic tests

--- CryptoHashTests/CryptoHashTests.swift
+++ CryptoHashTests/CryptoHashTests.swift
@@ -10,10 +10,10 @@ import XCTest
 @testable import CryptoHash
 
 class CryptoHashTests: XCTestCase {
-    
+    var data: Data!
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.data = "Hello World".data(using: .utf8)
     }
     
     override func tearDown() {
@@ -21,16 +21,25 @@ class CryptoHashTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testSHA1() {
+        let digest = self.data.SHA1()
+        let expected = Data([0x0a, 0x4d, 0x55, 0xa8, 0xd7, 0x78, 0xe5, 0x02, 0x2f, 0xab,
+                             0x70, 0x19, 0x77, 0xc5, 0xd8, 0x40, 0xbb, 0xc4, 0x86, 0xd0 ])
+        XCTAssertEqual(digest, expected)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    func testMD5() {
+        let digest = self.data.MD5()
+        let expected = Data([0xb1, 0x0a, 0x8d, 0xb1, 0x64, 0xe0, 0x75, 0x41,
+                             0x05, 0xb7, 0xa9, 0x9b, 0xe7, 0x2e, 0x3f, 0xe5])
+        XCTAssertEqual(digest, expected)
+    }
+
+    func testSHA256() {
+        let digest = self.data.SHA256()
+        let expected = Data([0xa5, 0x91, 0xa6, 0xd4, 0x0b, 0xf4, 0x20, 0x40, 0x4a, 0x01, 0x17, 0x33, 0xcf, 0xb7, 0xb1, 0x90, 0xd6, 0x2c, 0x65, 0xbf, 0x0b, 0xcd, 0xa3, 0x2b, 0x57, 0xb2, 0x77, 0xd9, 0xad, 0x9f, 0x14, 0x6e])
+        XCTAssertEqual(digest, expected)
+
     }
     
 }

48484fb24d085e2b4eabbed4846b9b0d178ce77e
Steve Brokaw <brokaw@me.com>
2017-12-20 13:24:33 -0600

    Refactored - Consolidated duplicated code into single method.

--- CryptoHash/Hashes.swift
+++ CryptoHash/Hashes.swift
@@ -2,38 +2,30 @@ import Foundation
 import CommonCrypto
 
 public extension Data {
-    public func MD5() -> Data {
-        return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
-            let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-            var buffer = [UInt8](repeating: 0, count: digestLen)
-            let dataLen = CC_LONG(self.count)
-            let _ = CC_MD5(pointer, dataLen, &buffer)
+    typealias  CCHashFunc = (UnsafeRawPointer?, CC_LONG, UnsafeMutablePointer<UInt8>?) -> UnsafeMutablePointer<UInt8>!
+
+    func hash(length: Int32, function: CCHashFunc) -> Data {
+        return self.withUnsafeBytes({ (pointer: UnsafePointer<UInt8>) -> Data in
+            var buffer = [UInt8](repeating: 0, count: Int(length))
+            let length = CC_LONG(self.count)
+            let _ = function(pointer, length, &buffer)
             return Data(bytes: buffer)
-        }
+        })
+    }
+    public func MD5() -> Data {
+        return hash(length: CC_MD5_DIGEST_LENGTH, function: CC_MD5)
     }
     public func MD5Hexstring() -> String {
         return self.MD5().hexstring()
     }
     public func SHA1() -> Data {
-        return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
-            let digestLen = Int(CC_SHA1_DIGEST_LENGTH)
-            var buffer = [UInt8](repeating: 0, count: digestLen)
-            let dataLen = CC_LONG(self.count)
-            let _ = CC_SHA1(pointer, dataLen, &buffer)
-            return Data(bytes: buffer)
-        }
+        return hash(length: CC_SHA1_DIGEST_LENGTH, function: CC_SHA1)
     }
     public func SHA1Hexstring() -> String {
         return self.SHA1().hexstring()
     }
     public func SHA256() -> Data {
-        return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
-            let digestLen = Int(CC_SHA256_DIGEST_LENGTH)
-            var buffer = [UInt8](repeating: 0, count: digestLen)
-            let dataLen = CC_LONG(self.count)
-            let _ = CC_SHA256(pointer, dataLen, &buffer)
-            return Data(bytes: buffer)
-        }
+        return hash(length: CC_SHA256_DIGEST_LENGTH, function: CC_SHA256)
     }
     public func SHA256Hexstring() -> String {
         return self.SHA256().hexstring()
@@ -47,3 +39,4 @@ extension Data {
         }.joined()
     }
 }
+

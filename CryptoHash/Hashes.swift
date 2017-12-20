cd21cef0c6c01eabedead4f3ff40bc9957a394db
Steve Brokaw <brokaw@me.com>
2017-12-19 22:27:25 -0600

    Moved code into closure.
    No functional changes.
    There was no reasoning behind what was declared in the method and what
    was declared in the closure.
    Moved everthing into the clousure

--- CryptoHash/Hashes.swift
+++ CryptoHash/Hashes.swift
@@ -3,11 +3,11 @@ import CommonCrypto
 
 public extension Data {
     public func MD5() -> Data {
-        let digestLen = Int(CC_MD5_DIGEST_LENGTH)
-        var buffer = [UInt8](repeating: 0, count: digestLen)
         return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
-            let length = CC_LONG(self.count)
-            let _ = CC_MD5(pointer, length, &buffer)
+            let digestLen = Int(CC_MD5_DIGEST_LENGTH)
+            var buffer = [UInt8](repeating: 0, count: digestLen)
+            let dataLen = CC_LONG(self.count)
+            let _ = CC_MD5(pointer, dataLen, &buffer)
             return Data(bytes: buffer)
         }
     }
@@ -15,11 +15,11 @@ public extension Data {
         return self.MD5().hexstring()
     }
     public func SHA1() -> Data {
-        let digestLen = Int(CC_SHA1_DIGEST_LENGTH)
-        var buffer = [UInt8](repeating: 0, count: digestLen)
         return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
-            let length = CC_LONG(self.count)
-            let _ = CC_SHA1(pointer, length, &buffer)
+            let digestLen = Int(CC_SHA1_DIGEST_LENGTH)
+            var buffer = [UInt8](repeating: 0, count: digestLen)
+            let dataLen = CC_LONG(self.count)
+            let _ = CC_SHA1(pointer, dataLen, &buffer)
             return Data(bytes: buffer)
         }
     }
@@ -27,11 +27,11 @@ public extension Data {
         return self.SHA1().hexstring()
     }
     public func SHA256() -> Data {
-        let digestLen = Int(CC_SHA256_DIGEST_LENGTH)
-        var buffer = [UInt8](repeating: 0, count: digestLen)
         return self.withUnsafeBytes { (pointer: UnsafePointer<UInt8>) -> Data in
-            let length = CC_LONG(self.count)
-            let _ = CC_SHA256(pointer, length, &buffer)
+            let digestLen = Int(CC_SHA256_DIGEST_LENGTH)
+            var buffer = [UInt8](repeating: 0, count: digestLen)
+            let dataLen = CC_LONG(self.count)
+            let _ = CC_SHA256(pointer, dataLen, &buffer)
             return Data(bytes: buffer)
         }
     }

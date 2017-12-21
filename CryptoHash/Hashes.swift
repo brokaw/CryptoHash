c089972085ef643500a4d8bfe203e05555e83c62
Steve Brokaw <brokaw@me.com>
2017-12-21 10:53:17 -0600

    Style changes.
    Removed IUO in typealias
    Removed unnecessary refernces to `self`
    Implemented linter feedback

--- CryptoHash/Hashes.swift
+++ CryptoHash/Hashes.swift
@@ -2,13 +2,13 @@ import Foundation
 import CommonCrypto
 
 public extension Data {
-    typealias  CCHashFunc = (UnsafeRawPointer?, CC_LONG, UnsafeMutablePointer<UInt8>?) -> UnsafeMutablePointer<UInt8>!
+    typealias  CCHashFunc = (UnsafeRawPointer, CC_LONG, UnsafeMutablePointer<UInt8>) -> UnsafeMutablePointer<UInt8>?
 
     func hash(length: Int32, function: CCHashFunc) -> Data {
-        return self.withUnsafeBytes({ (pointer: UnsafePointer<UInt8>) -> Data in
+        return withUnsafeBytes({ (pointer: UnsafePointer<UInt8>) -> Data in
             var buffer = [UInt8](repeating: 0, count: Int(length))
             let length = CC_LONG(self.count)
-            let _ = function(pointer, length, &buffer)
+            _ = function(pointer, length, &buffer)
             return Data(bytes: buffer)
         })
     }
@@ -16,43 +16,43 @@ public extension Data {
         return hash(length: CC_MD5_DIGEST_LENGTH, function: CC_MD5)
     }
     public func MD5Hexdigest() -> String {
-        return self.MD5Digest().hexstring()
+        return MD5Digest().hexstring()
     }
     public func SHA1Digest() -> Data {
         return hash(length: CC_SHA1_DIGEST_LENGTH, function: CC_SHA1)
     }
     public func SHA1Hexdigest() -> String {
-        return self.SHA1Digest().hexstring()
+        return SHA1Digest().hexstring()
     }
     public func SHA224Digest() -> Data {
         return hash(length: CC_SHA224_DIGEST_LENGTH, function: CC_SHA224)
     }
     public func SHA224Hexdigest() -> String {
-        return self.SHA224Digest().hexstring()
+        return SHA224Digest().hexstring()
     }
     public func SHA256Digest() -> Data {
         return hash(length: CC_SHA256_DIGEST_LENGTH, function: CC_SHA256)
     }
     public func SHA256Hexdigest() -> String {
-        return self.SHA256Digest().hexstring()
+        return SHA256Digest().hexstring()
     }
     public func SHA384Digest() -> Data {
         return hash(length: CC_SHA384_DIGEST_LENGTH, function: CC_SHA384)
     }
     public func SHA384Hexdigest() -> String {
-        return self.SHA384Digest().hexstring()
+        return SHA384Digest().hexstring()
     }
     public func SHA512Digest() -> Data {
         return hash(length: CC_SHA512_DIGEST_LENGTH, function: CC_SHA512)
     }
     public func SHA512Hexdigest() -> String {
-        return self.SHA512Digest().hexstring()
+        return SHA512Digest().hexstring()
     }
 }
 
 extension Data {
     func hexstring() -> String {
-        return self.map {
+        return map {
             String(format: "%02x", $0)
         }.joined()
     }
@@ -62,40 +62,39 @@ let ENCODING = String.Encoding.utf8
 
 public extension String {
     public func MD5Digest() -> Data? {
-        return self.data(using: ENCODING)?.MD5Digest()
+        return data(using: ENCODING)?.MD5Digest()
     }
     public func MD5Hexdigest() -> String? {
-        return self.data(using: ENCODING)?.MD5Hexdigest()
+        return data(using: ENCODING)?.MD5Hexdigest()
     }
     public func SHA1Digest() -> Data? {
-        return self.data(using: ENCODING)?.SHA1Digest()
+        return data(using: ENCODING)?.SHA1Digest()
     }
     public func SHA1Hexdigest() -> String? {
-        return self.data(using: ENCODING)?.SHA1Hexdigest()
+        return data(using: ENCODING)?.SHA1Hexdigest()
     }
     public func SHA224Digest() -> Data? {
-        return self.data(using: ENCODING)?.SHA224Digest()
+        return data(using: ENCODING)?.SHA224Digest()
     }
     public func SHA224Hexdigest() -> String? {
-        return self.data(using: ENCODING)?.SHA224Hexdigest()
+        return data(using: ENCODING)?.SHA224Hexdigest()
     }
     public func SHA256Digest() -> Data? {
-        return self.data(using: ENCODING)?.SHA256Digest()
+        return data(using: ENCODING)?.SHA256Digest()
     }
     public func SHA256Hexdigest() -> String? {
-        return self.data(using: ENCODING)?.SHA256Hexdigest()
+        return data(using: ENCODING)?.SHA256Hexdigest()
     }
     public func SHA384Digest() -> Data? {
-        return self.data(using: ENCODING)?.SHA384Digest()
+        return data(using: ENCODING)?.SHA384Digest()
     }
     public func SHA384Hexdigest() -> String? {
-        return self.data(using: ENCODING)?.SHA384Hexdigest()
+        return data(using: ENCODING)?.SHA384Hexdigest()
     }
     public func SHA512Digest() -> Data? {
-        return self.data(using: ENCODING)?.SHA512Digest()
+        return data(using: ENCODING)?.SHA512Digest()
     }
     public func SHA512Hexdigest() -> String? {
-        return self.data(using: ENCODING)?.SHA512Hexdigest()
+        return data(using: ENCODING)?.SHA512Hexdigest()
     }
 }
-

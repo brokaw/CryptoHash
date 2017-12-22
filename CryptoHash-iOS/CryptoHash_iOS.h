73000116fac8de790252cba034e08f46e241d6bf
Steve Brokaw <brokaw@me.com>
2017-12-21 18:38:02 -0600

    Added addtional target for iOS
    Starting support for cross platform building

---
+++ CryptoHash-iOS/CryptoHash_iOS.h
@@ -0,0 +1,19 @@
+//
+//  CryptoHash_iOS.h
+//  CryptoHash-iOS
+//
+//  Created by Steve Brokaw on 12/21/17.
+//  Copyright © 2017 Steve Brokaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CryptoHash_iOS.
+FOUNDATION_EXPORT double CryptoHash_iOSVersionNumber;
+
+//! Project version string for CryptoHash_iOS.
+FOUNDATION_EXPORT const unsigned char CryptoHash_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CryptoHash_iOS/PublicHeader.h>
+
+
